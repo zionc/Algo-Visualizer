366816ac3db43d88032479a0b5dccc80e17f2c80
zionc <zchilagan678@gmail.com>
2024-01-24 21:31:27 -0500

    Removed info, added DIRECTED/UNDIRECTED macro

--- tests/graphtest.c
+++ tests/graphtest.c
@@ -1,17 +1,14 @@
+#define UNDIRECTED
 #include "../include/graph.h"
 #include <stdio.h>
 #include <assert.h>
-
-
-typedef struct args{
-    int id;
-    char *label;
-} args;
+#include <stdlib.h>
 
 void test_create_node()
 {
+    
     Graph g = {0};
-    graph_init(&g,100,NULL);
+    graph_init(&g,100);
     Node node1 = graph_create_node(&g,10);
     Node node2 = graph_create_node(&g,30);
     Node node3 = graph_create_node(&g,10);
@@ -21,18 +18,21 @@ void test_create_node()
     assert(graph_node_pool_contains(&g,&node2));
     assert(graph_node_pool_contains(&g,&node3));
     graph_destroy(&g);
+    printf("test_create_node: Passed\n");
 }
+
 
 void test_graph_connect()
 {
     Graph g = {0};
-    graph_init(&g,100,NULL);
+    graph_init(&g,100);
     Node node1 = graph_create_node(&g,2);
     Node node2 = graph_create_node(&g,5);
     Node node3 = graph_create_node(&g,10);
     assert(node1.adjacent_size == 0);
     assert(node2.adjacent_size == 0);
     assert(node3.adjacent_size == 0);
+
 
     graph_connect(&g,&node1,&node2);
     graph_connect(&g,&node1,&node3);
@@ -56,15 +56,15 @@ void test_graph_connect()
     assert(g.nodes_pool_size == 3);
     assert(g.edges_pool_size == 4);
     
-
     graph_destroy(&g);
+    printf("test_graph_connect: Passed\n");
     
 }
 
 void test_node_equal()
 {
     Graph g = {0};
-    graph_init(&g,100,NULL);
+    graph_init(&g,100);
 
     Node node1 = graph_create_node(&g,5);
     Node node2 = graph_create_node(&g,5);
@@ -74,14 +74,10 @@ void test_node_equal()
     assert(!graph_node_equals(&node3,&node2));
 
     graph_destroy(&g);
+    printf("test_node_equal: Passed\n");
 }  
 
-// void test_graph_add_info() {
-//     Graph g = {0};
-//     graph_init(&g,500);
 
-//     graph_destroy(&g);
-// }
 
 int main(void) {
     test_create_node();
